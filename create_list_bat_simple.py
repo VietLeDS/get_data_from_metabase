0b3fc33ff474e9f62ed78a744eb073762308b99f
VietLeDS <106808758+VietLeDS@users.noreply.github.com>
2022-06-21 15:18:19 +0700

    Delete create_list_bat_simple.py

--- create_list_bat_simple.py
+++
@@ -1,9 +0,0 @@
-import os
-lst = [f for f in os.listdir() if '.bat' in f and 'Meta' in f]
-for _ in lst:
-	os.remove(_)
-
-lst = [f for f in os.listdir() if '.py' in f and 'Meta' in f]
-for _ in lst:
-	with open(_.replace('.py','.bat'), 'w') as f:
-		f.write(f'TITLE {_.replace(".py", "")}\npython {_}\nPAUSE')
